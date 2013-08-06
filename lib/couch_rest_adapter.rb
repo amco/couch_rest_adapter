d6e70e5c45eed24b99f0cd70de026b97eacc8464
javierg <javierg@amcoonline.net>
2013-08-06 15:44:30 -0700

    autoload instead of requiring railtie

--- lib/couch_rest_adapter.rb
+++ lib/couch_rest_adapter.rb
@@ -10,7 +10,7 @@ require File.join(File.dirname(__FILE__), '/exceptions/couch_rest_adapter')
 using CouchRestAdapter::Helpers
 
 module CouchRestAdapter
-  require 'couch_rest_adapter/railtie' if defined?(Rails)
+  autoload :Railtie, 'couch_rest_adapter/railtie'
 
   class Base < CouchRest::Document
     #TODO: As abstract class should not have any method definition
