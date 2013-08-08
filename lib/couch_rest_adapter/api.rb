35500fad295d6b245c0ce1d851fe03eeb51c9b4f
javierg <javierg@amcoonline.net>
2013-08-07 22:37:52 -0700

    force to use json/ext

--- lib/couch_rest_adapter/api.rb
+++ lib/couch_rest_adapter/api.rb
@@ -4,6 +4,7 @@ require 'couchrest/rest_api'
 module CouchRest
   module RestAPI
     def parse_response result, opts = {}
+      JSON.parser = JSON::Ext::Parser
       (opts.delete(:raw) || opts.delete(:head)) ? result : JSON.parse(result)
     end
   end
